4ae3d60f57ac95c51cc0a831b30217f452af19f3
fabiosimplis <fabio.simplis@gmail.com>
2024-01-26 15:32:30 -0300

    Adiciona teste Pilha

--- br/com/dio/testes/TestaPilha.java
+++ br/com/dio/testes/TestaPilha.java
@@ -1,4 +1,26 @@
 package br.com.dio.testes;
 
+import br.com.dio.no.No;
+import br.com.dio.pilha.Pilha;
+
 public class TestaPilha {
+
+    public static void main(String[] args) {
+        Pilha pilha = new Pilha();
+
+        pilha.push(new No("1"));
+        pilha.push(new No("2"));
+        pilha.push(new No("3"));
+        pilha.push(new No("4"));
+        pilha.push(new No("5"));
+        pilha.push(new No("6"));
+
+        System.out.println(pilha);
+
+        System.out.println(pilha.top());
+
+        System.out.println(pilha.pop().getConteudo());
+
+        System.out.println(pilha);
+    }
 }
